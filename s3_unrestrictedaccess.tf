33335a1794d9cf0de82b0432850c47a5d72bf1ca
ravicwp <109152895+ravicwp@users.noreply.github.com>
2022-07-27 13:17:16 +0530

    Update s3_unrestrictedaccess.tf

--- s3_unrestrictedaccess.tf
+++ s3_unrestrictedaccess.tf
@@ -6,6 +6,18 @@ resource "aws_s3_bucket" "delete1" {
   }
   logging {
     target_bucket = aws_s3_bucket.log_bucket.id
-    target_prefix = "log/"
+    target_prefix = "log"
+    }
+}
+
+resource "aws_s3_bucket" "delete2" {
+  bucket = "examplebuckettftest2"
+  acl    = "public-read-write"
+  versioning {
+    enabled = true
+  }
+  logging {
+    target_bucket = aws_s3_bucket.log_bucket.id
+    target_prefix = "log"
     }
 }
